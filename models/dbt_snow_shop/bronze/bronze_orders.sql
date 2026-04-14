46621a4b45376858f1a657c6f9344eb1cd8cd696
julian.buitrago88@gmail.com <julian.buitrago88@gmail.com>
2026-04-14 22:07:19 +0000

    minor update

--- models/dbt_snow_shop/bronze/bronze_orders.sql
+++ models/dbt_snow_shop/bronze/bronze_orders.sql
@@ -11,7 +11,6 @@ select
     $3::date    as order_date,
     $4::string  as status
 from @JAFFLE_SHOP.MY_INTERAL_STAGE/7lllisvro4xd-jaffle_shop_orders.csv
-
 (
   file_format => BRONZE.FF_CSV_SKIP_HEADER
 )
