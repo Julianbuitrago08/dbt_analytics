f4f66f3c87b3e88b922064ea2fddcb511b836815
julian.buitrago88@gmail.com <julian.buitrago88@gmail.com>
2026-04-15 19:30:20 +0000

    minor updtes regarding the process

--- models/dbt_snow_shop/bronze/bronze_customers.sql
+++ models/dbt_snow_shop/bronze/bronze_customers.sql
@@ -1,7 +1,7 @@
 {{
   config(
     tag = ['jaffle_shop'],
-    shema = 'BRONZE',
+    shema = 'bronze',
     materialized='table'
   ) 
 }}
@@ -9,7 +9,11 @@
 select
     $1::integer  as id,
     $2::string   as first_name,
-    $3::string   as last_name
+    $3::string   as last_name,
+    metadata$filename             as source_file_name,
+    metadata$file_last_modified   as file_last_modified_ts,
+    metadata$start_scan_time      as load_timestamp
+
 from @BRONZE.MY_INTERAL_STAGE/rv8ef6lvb2uh-jaffle_shop_customers.csv
 (
   file_format => BRONZE.FF_CSV_SKIP_HEADER
