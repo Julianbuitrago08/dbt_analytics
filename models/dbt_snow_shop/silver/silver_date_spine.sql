a74a768b5420f0744b96ddd4dc7df2873e71e623
julian.buitrago88@gmail.com <julian.buitrago88@gmail.com>
2026-04-20 16:25:16 +0000

    minor update to add doc genetate and other minor updates

---
+++ models/dbt_snow_shop/silver/silver_date_spine.sql
@@ -0,0 +1,14 @@
+{{
+  config(
+    tags = 'jaffa_shop',
+    schema = 'silver',
+    materialized='table',
+  ) 
+}}
+
+{{ dbt_utils.date_spine(
+    datepart="day",
+    start_date="cast('2026-01-01' as date)",
+    end_date="cast('2027-01-01' as date)"
+   )
+}}
