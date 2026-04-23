57927483ee8b5ea42303f92c9d6d3d1ea934d3bc
julian.buitrago88@gmail.com <julian.buitrago88@gmail.com>
2026-04-23 15:55:58 +0000

    new tables and new logic

--- models/dbt_snow_shop/silver/silver_orders_customer.sql
+++ models/dbt_snow_shop/silver/silver_orders_customer.sql
@@ -13,39 +13,49 @@ with customers as (
         first_name,
         last_name
     from {{ ref('bronze_customers') }}
+
 ),
+
 orders as (
+
     select
-        id        as order_id,
-        user_id   as customer_id,
+        id      as order_id,
+        user_id as customer_id,
         order_date,
-        status    as order_status
+        status  as order_status
     from {{ ref('bronze_orders') }}
 
 ),
+
 payments as (
+
     select 
-        id      as payment_id,
         orderid as order_id,
-        sum (case when payment_status = 'success' then amount end) as amount
+        sum(
+            case 
+                when payment_status <> 'fail' then amount 
+                else 0 
+            end
+        ) as amount_cents
     from {{ ref('bronze_payment') }}
-    group by 1,2
+    group by orderid
 
 ),
+
 final as (
+
     select
         o.order_id,
         c.customer_id,
-        sum(pa.amount) as lifetime_value
+        {{ cents_to_dollars('coalesce(p.amount_cents, 0)') }} as lifetime_value
     from customers c
     left join orders o
         on c.customer_id = o.customer_id
-    left join payments pa
-        on o.order_id = pa.order_id
-    group by
-        o.order_id,
-        c.customer_id
+    left join payments p
+        on o.order_id = p.order_id
+
 )
+
 select *
 from final
 where order_id is not null
