a3fece2b228687ce114c2637fa86b4f4658720db
Julianbuitrago08 <78058485+Julianbuitrago08@users.noreply.github.com>
2026-04-27 16:30:19 -0500

    Update silver_customer_orders_legacy.sql

--- models/dbt_snow_shop/silver/silver_customer_orders_legacy.sql
+++ models/dbt_snow_shop/silver/silver_customer_orders_legacy.sql
@@ -14,23 +14,23 @@ WITH paid_orders as (select Orders.ID as order_id,
         p.payment_finalized_date,
         C.FIRST_NAME    as customer_first_name,
         C.LAST_NAME as customer_last_name
-    FROM {{ref('bronze_orders')}} as Orders
+    FROM {{ref('bronze_jaffashop_orders')}} as Orders
     left join ( select 
                     ORDERID as order_id, max(CREATED) as payment_finalized_date, 
                     {{ cents_to_dollars('coalesce(sum(AMOUNT), 0)') }} as total_amount_paid
-                from {{ref('bronze_payment')}}
+                from {{ref('bronze_jaffashop_payment')}}
                 where payment_status <> 'fail'
                 group by 1) p 
         ON orders.ID = p.order_id
-left join {{ref('bronze_customers')}} C on orders.USER_ID = C.ID )
+left join {{ref('bronze_jaffashop_customers')}} C on orders.USER_ID = C.ID )
 ,
 customer_orders 
     as (select C.ID as customer_id
         , min(ORDER_DATE) as first_order_date
         , max(ORDER_DATE) as most_recent_order_date
         , count(ORDERS.ID) AS number_of_orders
-    from {{ref('bronze_customers')}} C 
-    left join {{ref('bronze_orders')}} as Orders
+    from {{ref('bronze_jaffashop_customers')}} C 
+    left join {{ref('bronze_jaffashop_orders')}} as Orders
         on orders.USER_ID = C.ID 
     group by 1)
 
